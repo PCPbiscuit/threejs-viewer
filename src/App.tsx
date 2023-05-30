2aefa5a0379c1fc124a0cd9ec513a4c3e36e3332
Vladimir Shadrov <vovashadrov@yandex.ru>
2023-05-30 22:17:12 +0300

    try without any extra content

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       <Canvas
         rotate={allowRotation}
         ref={ref}
-        className='!fixed left-0 top-0 w-full h-full'
+        // className='!fixed left-0 top-0 w-full h-full'
       >
         <XR>
           <Controllers />
@@ -42,53 +42,57 @@ function App() {
           enableRotation={allowRotation}
         /> */}
       </Canvas>
-      <main className='px-8 py-11 h-full bg-gradient-to-r from-[#201B18] to-[#7C6C60]'>
-        <div className='grid grid-cols-3 h-full'>
-          <div
-            className={clsx(
-              'flex flex-col justify-between col-span-2 text-black',
-            )}
-          >
-            <Logo />
+      {false && (
+        <main className='px-8 py-11 h-full bg-gradient-to-r from-[#201B18] to-[#7C6C60]'>
+          <div className='grid grid-cols-3 h-full'>
             <div
-              className='w-[60px] h-[60px] rounded-full bg-white flex items-center justify-center shrink-0 cursor-pointer hover:scale-105 transition-transform transform font-bold'
-              onClick={handleAllowRotation}
+              className={clsx(
+                'flex flex-col justify-between col-span-2 text-black',
+              )}
             >
-              {allowRotation ? 'R' : <RotateIcon />}
+              <Logo />
+              <div
+                className='w-[60px] h-[60px] rounded-full bg-white flex items-center justify-center shrink-0 cursor-pointer hover:scale-105 transition-transform transform font-bold'
+                onClick={handleAllowRotation}
+              >
+                {allowRotation ? 'R' : <RotateIcon />}
+              </div>
             </div>
-          </div>
-          <div className={clsx('flex flex-col space-y-4 text-white z-10')}>
-            <div className='rounded-[14px] bg-white bg-opacity-10 h-full flex flex-col p-10 justify-between'>
-              <div>
-                <img
-                  src='/shtule.png'
-                  className='w-full rounded-lg overflow-hidden h-60 mb-6 object-cover'
-                />
-                <h2 className='font-bold text-lg mb-5'>Стул Style DSW серый</h2>
-                <p className='font-medium'>
-                  Знаменитый образец современного дизайна в стиле минимализм.
-                  Дизайнеры и декораторы с неизменным успехом используют стул
-                  DSW в гостиных, кухнях, кабинетах, детских комнатах, на
-                  террасах и в публичных помещениях.
-                </p>
+            <div className={clsx('flex flex-col space-y-4 text-white z-10')}>
+              <div className='rounded-[14px] bg-white bg-opacity-10 h-full flex flex-col p-10 justify-between'>
+                <div>
+                  <img
+                    src='/shtule.png'
+                    className='w-full rounded-lg overflow-hidden h-60 mb-6 object-cover'
+                  />
+                  <h2 className='font-bold text-lg mb-5'>
+                    Стул Style DSW серый
+                  </h2>
+                  <p className='font-medium'>
+                    Знаменитый образец современного дизайна в стиле минимализм.
+                    Дизайнеры и декораторы с неизменным успехом используют стул
+                    DSW в гостиных, кухнях, кабинетах, детских комнатах, на
+                    террасах и в публичных помещениях.
+                  </p>
+                </div>
+                <div className='font-bold cursor-pointer'>Купить на сайте </div>
+                <ARButton />
               </div>
-              <div className='font-bold cursor-pointer'>Купить на сайте </div>
-              <ARButton />
-            </div>
-            <div className='bg-white h-24 flex items-center justify-center space-x-6 rounded-xl'>
-              <div className='border border-[#dcdcdc] rounded-full h-[60px] w-[60px] flex items-center justify-center shrink-0'>
-                <CameraIcon />
-              </div>
-              <div className='border border-[#dcdcdc] rounded-full h-[60px] w-[60px] flex items-center justify-center shrink-0'>
-                <ShareIcon />
-              </div>
-              {/* <div className='border border-[#dcdcdc] bg-red-600 rounded-full h-[60px] w-[60px] flex items-center justify-center shrink-0'>
+              <div className='bg-white h-24 flex items-center justify-center space-x-6 rounded-xl'>
+                <div className='border border-[#dcdcdc] rounded-full h-[60px] w-[60px] flex items-center justify-center shrink-0'>
+                  <CameraIcon />
+                </div>
+                <div className='border border-[#dcdcdc] rounded-full h-[60px] w-[60px] flex items-center justify-center shrink-0'>
+                  <ShareIcon />
+                </div>
+                {/* <div className='border border-[#dcdcdc] bg-red-600 rounded-full h-[60px] w-[60px] flex items-center justify-center shrink-0'>
                 <XIcon />
               </div> */}
+              </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+      )}
     </>
   );
 }
