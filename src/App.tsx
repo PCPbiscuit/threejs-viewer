74c14b9f8ec569eddfbab4aa824eaa30615dce53
Vladimir Shadrov <vovashadrov@yandex.ru>
2023-06-25 19:45:54 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import { ReactComponent as RotateIcon } from './assets/360.svg';
 import { ReactComponent as ShareIcon } from './assets/share.svg';
 import { ReactComponent as CameraIcon } from './assets/camera.svg';
 import { ReactComponent as FloraIcon } from './assets/flora.svg';
+import { ReactComponent as PhoneIcon } from './assets/phone.svg';
+import { ReactComponent as WebIcon } from './assets/web.svg';
 import { Canvas, Model } from './ui';
 
 const models = [
@@ -72,7 +74,7 @@ function App() {
     });
   }, []);
   return (
-    <div className='md:h-screen bg-gradient-to-r from-[#201B18] to-[#7C6C60]'>
+    <div className='md:h-screen bg-neutral-50 md:bg-gradient-to-r md:from-[#201B18] md:to-[#7C6C60]'>
       {window.innerWidth >= 768 && (
         <Canvas
           rotate={allowRotation}
@@ -92,7 +94,7 @@ function App() {
               'md:flex hidden flex-col justify-between md:col-span-2 text-black',
             )}
           >
-            <Logo />
+            <Logo className='text-white' />
             <div
               className='w-[60px] h-[60px] rounded-full bg-white flex items-center justify-center shrink-0 cursor-pointer hover:scale-105 transition-transform transform font-bold'
               onClick={handleAllowRotation}
@@ -101,10 +103,27 @@ function App() {
             </div>
           </div>
           <div className={clsx('flex flex-col space-y-4 text-white z-10')}>
-            <div className='flex items-center justify-between space-x-4'>
-              <Logo /> <FloraIcon />
+            <div className='md:hidden flex flex-col space-y-6'>
+              <Logo className='text-[#5C5C5C] md:text-white' />
+              <div className='bg-white rounded-xl py-2.5 px-4 flex items-center space-x-3'>
+                <div className='py-2.5 text-black px-2 aspect-square flex-col space-y-1 rounded-full items-ceter flex justify-center border border-[#DCDCDC]'>
+                  <img
+                    src='/flora.png'
+                    width='32px'
+                    height='28px'
+                    className='object-cover self-center w-8'
+                  />
+                  <span className='text-[10px]'>АРТФЛОРА</span>
+                </div>
+                <div className='flex flex-col space-y-1 font-medium text-black'>
+                  <span className='text-xs text-black text-opacity-50'>
+                    Продавец
+                  </span>
+                  <span className='text-xl'>Артфлора</span>
+                </div>
+              </div>
             </div>
-            <div className='rounded-[14px] bg-white bg-opacity-10 h-full flex flex-col md:p-10 p-4 justify-between z-50'>
+            <div className='rounded-[14px] bg-white md:bg-opacity-10 h-full flex flex-col md:p-10 p-6 justify-between z-50'>
               <div className='hidden md:block'>
                 <img
                   src='/shtule.png'
@@ -119,17 +138,18 @@ function App() {
                 </p>
               </div>
               <div className='font-bold cursor-pointer hidden md:block'>
-                Купить на сайте{' '}
+                Купить на сайте
               </div>
-              <div className='relative'>
-                <div className='grid grid-cols-2 gap-4 '>
-                  {models.map((model, i) => (
-                    <img
-                      src={model.preview}
-                      className='aspect-square object-cover'
-                      onClick={handleModel(i)}
-                    />
-                  ))}
+              <div className='relative md:hidden'>
+                <div className='flex flex-col'>
+                  <span className='bg-[#FFAC0C21] rounded-full py-1 px-3 text-xs font-medium text-[#FF9E0C] w-fit'>
+                    Группа
+                  </span>
+                  <p className='font-medium text-lg mt-1.5'>Стулья DSW</p>
+                  <img
+                    className='aspect-[5/4] w-full rounded-md'
+                    src={models[0].preview}
+                  />
                 </div>
                 <model-viewer
                   id='test'
@@ -137,7 +157,7 @@ function App() {
                   ar
                   // ar-modes='scene-viewer webxr'
                   camera-controls
-                  class='w-full'
+                  class='w-full h-0'
                   src={models[currentModelIndex]?.model}
                   reveal={arStarted ? 'auto' : 'manual'}
                   alt='A 3D shoes'
@@ -160,7 +180,7 @@ function App() {
                   {arStarted && (
                     <>
                       <div className='flex w-full absolute top-0 left-0 items-center justify-between space-x-4 p-10'>
-                        <Logo /> <FloraIcon />
+                        <Logo className='text-white' /> <FloraIcon />
                       </div>
                       <div className='absolute w-full top-1/2 flex items-center justify-center -translate-y-1/2'>
                         {models[currentModelIndex]?.title}
@@ -191,14 +211,15 @@ function App() {
               </div>
             </div>
             <div className='md:hidden flex flex-col space-y-2 text-sm'>
-              <button className='bg-white p-5 text-black'>
-                Открыть в мобильном приложении
-              </button>
               <button
-                className='bg-white bg-opacity-25 p-5'
+                className='bg-[#1B1B1B] p-4 flex items-center justify-center'
                 onClick={activateAR}
               >
-                Открыть в браузере
+                <WebIcon className='mr-2.5' /> <span>Открыть в браузере</span>
+              </button>
+              <button className='bg-[#745CF6] p-4 flex items-center justify-center'>
+                <PhoneIcon className='mr-2.5' />{' '}
+                <span>Открыть в приложении</span>
               </button>
             </div>
             <div className='bg-white h-24 md:flex hidden items-center justify-center space-x-6 rounded-xl'>
